f69a86bfadec7d1daee52c23835dc7def367f529
Dan Alvidrez <dan@bayareawebpro.com>
2020-05-04 19:48:29 -0700

    Update ProviderTest.php

--- tests/Unit/ProviderTest.php
+++ tests/Unit/ProviderTest.php
@@ -3,6 +3,7 @@
 namespace BayAreaWebPro\PackageName\Tests\Unit;
 
 use BayAreaWebPro\PackageName\PackageName;
+use BayAreaWebPro\PackageName\PackageNameService;
 use BayAreaWebPro\PackageName\PackageNameServiceProvider;
 use BayAreaWebPro\PackageName\Tests\TestCase;
 
@@ -18,31 +19,33 @@ class ProviderTest extends TestCase
     public function test_container_can_resolve_instance()
     {
         $this->assertInstanceOf(
-            PackageName::class,
+            PackageNameService::class,
             $this->app->make('package-name'), 'Container can make instance of service.');
     }
 
     public function test_facade_can_resolve_instance()
     {
         $this->assertInstanceOf(
-            PackageName::class,
+            PackageNameService::class,
             \PackageName::getFacadeRoot(), 'Alias class can make instance of service.');
 
         $this->assertInstanceOf(
-            PackageName::class,
-            \BayAreaWebPro\PackageName\PackageName::getFacadeRoot(), 'Facade can make instance of service.');
+            PackageNameService::class,
+            PackageName::getFacadeRoot(), 'Facade can make instance of service.');
     }
 
     public function test_service_can_be_resolved()
     {
         $instance = app('package-name');
-        $this->assertTrue($instance instanceof PackageName);
+        $this->assertTrue($instance instanceof PackageNameService);
     }
 
     public function test_declares_provided()
     {
         $this->assertTrue(in_array('package-name',
-                collect(app()->getProviders(PackageNameServiceProvider::class))->first()->provides())
-        );
+                collect(app()->getProviders(PackageNameServiceProvider::class))
+                ->first()
+                ->provides()
+        ));
     }
 }
