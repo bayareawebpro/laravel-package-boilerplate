4667ba4a93cac85192cda6d0898de811a078bce7
Dan Alvidrez <dan@bayareawebpro.com>
2025-05-01 00:14:55 -0700

    Update UserFactory.php

--- workbench/database/factories/UserFactory.php
+++ workbench/database/factories/UserFactory.php
@@ -3,26 +3,24 @@
 namespace Workbench\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 use Workbench\App\Models\User;
 
 /**
- * @template TModel of \Workbench\App\User
- *
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<TModel>
+ * @template TModel of User
+ * @extends Factory<TModel>
  */
 class UserFactory extends Factory
 {
     /**
      * The name of the factory's corresponding model.
-     *
      * @var class-string<TModel>
      */
     protected $model = User::class;
 
     /**
      * Define the model's default state.
-     *
      * @return array<string, mixed>
      */
     public function definition(): array
@@ -31,7 +29,7 @@ class UserFactory extends Factory
             'name' => $this->faker->name,
             'email' => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
     }
